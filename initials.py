8d1455f5bbfeefe984d87ce7b9fcf6b28c71fb4d
Rosemary Awoenam Yevoo <rosemaryyevoo@gmail.com>
2026-05-31 18:00:42 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#File:initials.py
+#Description: Printing out my initials
+#Assignment Number: Assignment Number, 1
+#
+#Name:  Rosemary Awoenam Yevoo
+#STUDENT ID: 2425402524
+#Email: 2425402524@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor,Rosemary Awoenam Yevoo, this programming assignment is my own work
+#I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+  #Program to print out my initials
+    print()
+    print("...RAY")
+    print()
+    print("...RRRRRRRRR...............AAAA............YY........YY.....")
+    print("...RR.......RR............AA..AA............YY......YY......")
+    print("...RR........RR..........AA....AA............YY....YY.......")
+    print("...RR.......RR..........AA......AA............YY..YY........")
+    print("...RRRRRRRRR............AAAAAAAAAA.............YYYY.........")
+    print("...RRRRRRRRR............AAAAAAAAAA.............YYYY.........")
+    print("...RR.....RR...........AA........AA............YYYY.........")
+    print("...RR......RR..........AA........AA............YYYY.........")
+    print("...RR.......RR....**...AA........AA...**.......YYYY.......**")
+    print("...RR........RR...**...AA........AA...**.......YYYY.......**")
 
 main()
+
+
